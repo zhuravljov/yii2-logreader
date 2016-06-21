1c97cd0590cd5ffe1544c09a1bbb5e453cbc297b
Roman Zhuravlev <zhuravljov@gmail.com>
2016-06-21 19:51:35 +0300

    Module skeleton

---
+++ src/Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace zhuravljov\yii\logreader;
+
+/**
+ * LogReader module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'zhuravljov\yii\logreader\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
